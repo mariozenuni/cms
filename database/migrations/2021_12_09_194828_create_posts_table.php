bc94fa9987351656d5ba1884dc690552ae39428d
Mario Zenuni <mariozenuni@gmail.com>
2021-12-09 20:15:13 +0000

    authentication system and Created Post and Category model

---
+++ database/migrations/2021_12_09_194828_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title',10);
+            $table->string('description',20);
+            $table->text('content',255);
+            $table->string('image',255);
+            $table->timestamps('published_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
